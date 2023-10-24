023f08c1b685df968eb8225f32acd690929fe1f5
Vipin Mathew <vipin.mathew@sleevesup.com.au>
2023-10-24 17:46:23 +0530

    fix node modules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-import { SnotifyPosition } from 'ng-snotify';
+// import { SnotifyPosition } from 'ng-snotify';
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
@@ -10,8 +10,7 @@ export const environment = {
   baseUrl : `http://localhost:4200/`,
 
   snotifyConfig : {
-    showProgressBar: true,
-    position: SnotifyPosition.rightTop,
+    showProgressBar: true
   },
 };
 
